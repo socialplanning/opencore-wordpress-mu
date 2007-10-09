92b6b7363314988d1f35c835049aea32d8a3c87c
Anil Makhijani <amakhijani@gmail.com>
2007-10-09 15:24:44 +0000

    I added the file op_syncusers.php which will be used as a one time sync between op users and wp users.  it uses all.xml to populate the wp_users db on the wordpress side of  things.
    git-svn-id: https://svn.socialplanning.org/svn/build/topp.build.wordpress/trunk/topp/build/wordpress/wordpress-mu@9361 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ op_syncusers.php
@@ -0,0 +1,98 @@
+<?php
+
+$url_members = "http://anil:4570/openplans/people/all.xml";
+
+class user_profile
+{
+  var $username = '';
+  var $name = '';
+  var $password = '';
+  var $emailaddress = '';
+}
+
+require('wp-config.php');
+require('wp-includes/registration.php');
+require_once('Snoopy.class.php');
+
+
+$resp = _fetch_remote_file( $url_members, "admin", "admin" );
+$users = _parse_user_file($resp->results);
+update_wp_users_table($users);
+
+function update_wp_users_table($users)
+{
+  foreach ($users as $user)
+    {
+      wpmu_create_user($user->username, $user->password, $user->emailaddress);
+      //echo "added user: <br>";
+      //print_user($user);
+    }
+}
+
+function _parse_user_file ($data)
+{
+  if (! ($xmlparser = xml_parser_create() ) )
+    {
+      die ("Cannot create parser");
+    }
+
+  $data=eregi_replace(">"."[[:space:]]+"."<","><",$data);
+  xml_parse_into_struct($xmlparser, $data, $vals, $index);
+
+  $users = array();
+
+  $currentSize = 0;
+
+  foreach ($vals as $val)
+    {
+      if ($val[type] == "complete")
+	{
+	  if ($val[tag] == "USERNAME")
+	    {
+	      array_push($users, new user_profile() );
+	      $users[$currentSize]->username = $val[value];
+	    }
+	  if ($val[tag] == "NAME")
+	    {
+	      $users[$currentSize]->name = $val[value];
+	    }
+	  if ($val[tag] == "EMAIL")
+	    {
+	      $users[$currentSize]->emailaddress = $val[value];
+	      $currentSize = $currentSize+1;
+	    }
+	}
+    }
+
+  return $users;
+}
+
+function _fetch_remote_file ($url, $username, $password,  $headers = "" )
+{
+  // Snoopy is an HTTP client in PHP
+  $client = new Snoopy();
+  $client->user = "admin";
+  $client->pass = "admin";
+  $client->agent = MAGPIE_USER_AGENT;
+  $client->read_timeout = MAGPIE_FETCH_TIME_OUT;
+  $client->use_gzip = MAGPIE_USE_GZIP;
+  if (is_array($headers) )
+    {
+      $client->rawheaders = $headers;
+    }
+  
+  @$client->fetch($url);
+  return $client;
+}
+
+function print_user($user)
+{
+  echo "<br>-----------------------<br>";
+  echo "username: ".$user->username.'<br>';
+  echo "name: ".$user->name.'<br>';
+  echo "email: ".$user->emailaddress.'<br>';
+  echo "password: ".$user->password.'<br>';
+  echo "-----------------------<br>";
+}
+
+?>
--- openplans-create-blog.php
+++ openplans-create-blog.php
@@ -6,7 +6,6 @@
  *   domain = domain to create
  *   path = path to create (e.g., '/blog')
  *   title = title of blog
- *   user_id = user id of first admin of site (?)
  *   signature = hmac of domain, plus secret key, to validate request
  * Response:
  *   'ok'
@@ -15,6 +14,7 @@ define('WP_INSTALLING', true);
 require_once('openplans-auth.php');
 require_once('wp-config.php');
 require_once(ABSPATH . WPINC . '/wpmu-functions.php');
+require_once('Snoopy.class.php');
 
 $sig = $_POST['signature'];
 
@@ -22,36 +22,187 @@ $domain = $_POST['domain'];
 $secret = get_openplans_secret();
 $expect = hash_hmac("sha1", $domain, $secret, true);
 $expect = trim(base64_encode($expect));
-if ($sig != $expect) {
-    die("Signature '$sig' invalid for domain '$domain'");
+echo $domain."<br>";
+{
+  die("Signature '$sig' invalid for domain '$domain'");
 }
 
 $path = $_POST['path'];
-if (! $path) {
-    $path = '/blog';
+
+if (!$path) 
+{
+  $path = '/blog';
 }
 
 $title = $_POST['title'];
-/* FIXME: not sure what user_id should even be, or if we really need it
-   in this case */
-$user_id = $_POST['user_id'];
 
 
+//This makes sure that a blog doesn't already exist with the name that
+//is requested
 $blogs = $wpdb->get_results( "SELECT blog_id FROM $wpdb->blogs WHERE domain = '$domain'", ARRAY_A);
 
-if ($blogs) {
-	header("Status: 400 Bad Request");
-	echo "Blog with domain '$domain' already exists, id={$blogs[0][blog_id]}";
-	exit(0);
+if ($blogs)
+{
+  header("Status: 400 Bad Request");
+  echo "Blog with domain '$domain' already exists, id={$blogs[0][blog_id]}: ";
+  exit(0);
 }
 
 
-$blog_id = wpmu_create_blog($domain, $path, $title, $user_id);
-
-if (! $blog_id) {
-	header("Status: 500 Server Error");
-	echo("Error creating blog");
-} else {
-	echo("Created blog ID $blog_id");
+############CLASS DEFINITION############
+class member_profile
+{
+  var $username = "";
+  var $teams = array(); 
+  # key is team name, value is an array of roles
 }
+###########END CLASS DEF################
+
+$domain_pieces = split("\.", $domain);
+$project_name = $domain_pieces[0];
+$url_team = "http://anil:4570/openplans/projects/".$project_name."/members.xml";
+
+//echo "The project that was selected from openplans is $project_name: ";
+
+$team = array();
+$resp = _fetch_remote_file( $url_team, "admin", "admin" );
+$team = _parse_team_file($resp->results);
+
+//check to see if all the users are in the wp table and find the first
+//administrator
+
+$firstAdmin = '';
+$firstAdminUserID;
+
+$currentUserNum = 0;
+
+foreach ($team as $user)
+{
+  $userID = $wpdb->get_row( "SELECT ID FROM $wpdb->users WHERE user_login = '$user->username'");
+  if (! $userID)
+    {
+      header("Status: 400 Bad Request");
+      echo "User with name $user->username does not have a blog username! :";
+      exit(0);
+    }
+  
+  if ($firstAdmin == '' && ($user->teams[$project_name][0] == "ProjectAdmin") )
+    {
+      $firstAdmin = $user->username;
+      $firstAdminUserID = $wpdb->get_row( "SELECT ID FROM $wpdb->users WHERE user_login = '$user->username'");
+      unset($team[$currentUserNum]);
+      echo "The first admin found is $firstAdmin :";
+    }
+  $currentUserNum = $currentUserNum + 1;
+}
+
+echo "Creating the blog with just this admin user for now $firstAdmin with id $firstAdminUserID->ID: ";
+$blog_id = wpmu_create_blog($domain, $path, $title, $firstAdminUserID->ID);
+
+if (!$blog_id) 
+{
+  header("Status: 500 Server Error");
+  echo("Error creating blog");
+}
+else 
+{
+  echo("Created blog ID $blog_id : ");
+}
+
+//echo "Now adding the rest of the team to the blog :";
+
+//add each user to allow to work on the blog
+foreach ($team as $user)
+{
+  $userID = $wpdb->get_row( "SELECT ID FROM $wpdb->users WHERE user_login = '$user->username'");
+  $op_role =  $user->teams[$project_name][0];
+  $wp_role = '';
+  
+  if ($op_role === "ProjectMember")
+    {
+      $wp_role = 'author';
+    }
+  if ($op_role === "ProjectAdmin")
+    {
+      $wp_role = 'administrator';
+    }
+  
+  //echo "Adding the user $user->username to the blog :";
+  add_user_to_blog($blog_id, $userID->ID, $wp_role);
+  
+}
+
+
+###############FUNCTIONS DEFS#####################
+
+function _fetch_remote_file ($url, $username, $password,  $headers = "" )
+	{
+	  // Snoopy is an HTTP client in PHP
+	  $client = new Snoopy();
+	  $client->user = "admin";
+	  $client->pass = "admin";
+	  $client->agent = MAGPIE_USER_AGENT;
+	  $client->read_timeout = MAGPIE_FETCH_TIME_OUT;
+	  $client->use_gzip = MAGPIE_USE_GZIP;
+	  if (is_array($headers) )
+	    {
+	      $client->rawheaders = $headers;
+	    }
+	  
+	  @$client->fetch($url);
+	  return $client;
+	}
+
+
+
+function _parse_team_file ($data)
+{
+  global $project_name;
+  
+  $return_team = array();
+  if (! ($xmlparser = xml_parser_create()) )
+    { 
+      die ("Cannot create parser");
+    }
+  else
+    {
+    }		
+  
+  
+  $data=eregi_replace(">"."[[:space:]]+"."<","><",$data);
+  
+  xml_parse_into_struct($xmlparser, $data, $vals, $index);
+  
+  foreach ($vals as $val)
+    {
+      if ($val[type] == "open" || $val[type] == "complete")
+	{
+	  
+	  $currentMemberNumber = sizeof ($return_team) - 1;
+	  $currentTeamNumber = sizeof ( $return_team[$currentMemberNumber] ) - 1;
+	  
+	  switch ($val[tag])
+	    {
+	    case ("MEMBER"):
+	      array_push($return_team, new member_profile());
+	      break;
+	    case ("ID"):
+	      $return_team[$currentMemberNumber]->username = $val[value];
+	      $return_team[$currentMemberNumber]->teams[$project_name] = array();
+	      break;
+	    case ("ROLE"):
+	      array_push($return_team[$currentMemberNumber]->teams[$project_name], $val[value]);
+	      break; 
+	      
+	    }
+	}
+      
+    }
+  
+  xml_parser_free($xmlparser);	
+  
+  return $return_team;
+}
+###############END FUNCTIONS DEFS#####################
+
 ?>
