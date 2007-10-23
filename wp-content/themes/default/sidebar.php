b541c355db232b50cf0d6a458140251680dec831
Nick Grossman <nick@wrkng.net>
2007-10-23 19:27:56 +0000

    added "write post" buttons to WP sidebar
    git-svn-id: https://svn.socialplanning.org/svn/build/topp.build.wordpress/trunk/topp/build/wordpress/wordpress-mu@9803 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-content/themes/default/sidebar.php
+++ wp-content/themes/default/sidebar.php
@@ -4,6 +4,18 @@
 
 <?php   /* Widgetized sidebar, if you have the plugin installed. */
     if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+    
+  <?php if (current_user_can('publish_posts')) : ?>
+    <a href="wp-admin/post-new.php" class="oc-banana">Write a post</a>
+    <p style="text-align: center">
+      <a href="wp-admin/">Blog admin</a>
+    </p>
+  <?php elseif (current_user_can('edit_posts')) : ?>
+    <a href="wp-admin/post-new.php" class="oc-banana">Submit a post</a>
+    <p style="text-align: center">
+      <a href="wp-admin/">Blog admin</a>
+    </p>
+  <?php endif; ?>
 
   <div id="search" class="oc-boxy">
    <form id="searchform" method="get" action="<?php bloginfo('home'); ?>">
@@ -14,14 +26,7 @@
     </span>
   </form>
   </div>
-  
-  <div class="oc-boxy">
-    <h2>Pages</h2>
-    <ul class="oc-plainList">
-    <?php wp_list_pages('title_li='); ?>
-    </ul>
-  </div>
-  
+    
   <div class="oc-boxy">
     <h2>Categories</h2>
     <ul class="oc-plainList">
@@ -36,13 +41,12 @@
   </div>
 
   <div class="oc-boxy oc-plainList">
-    <?php wp_list_bookmarks('title_li='); ?>
+ 	  <?php wp_list_bookmarks('title_li='); ?>
   </div>
   
   <div id="meta" class="oc-boxy">
     <h2><?php _e('Feeds'); ?></h2>
     <ul class="oc-plainList">
-      <?php wp_register(); ?>
       <li><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><?php _e('Articles <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
       <li><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php _e('The latest comments to all posts in RSS'); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
       <?php wp_meta(); ?>
