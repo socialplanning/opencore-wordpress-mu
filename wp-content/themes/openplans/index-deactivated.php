e88052a92cd0ece935c70e6182a422b9d7e7060a
Nick Grossman <nick@wrkng.net>
2007-11-04 17:57:05 +0000

    copy edits to the deactivated-blog error page
    git-svn-id: https://svn.socialplanning.org/svn/build/topp.build.wordpress/trunk/topp/build/wordpress/wordpress-mu@10402 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-content/themes/openplans/index-deactivated.php
+++ wp-content/themes/openplans/index-deactivated.php
@@ -6,7 +6,16 @@ get_header();
 
 
 <h1>
-Sorry! This project does not have a blog.
+Sorry!
+</h1>
+
+<?php if (!current_user_can('publish_posts')) :?>
+  <p>The blog for this project has been disabled.  You can enable it by updating your <a href="../preferences">project preferences.</a></p>
+<?php else:  ?>
+  <p>There is no blog associated with this project.  Return to the <a href="../">project homepage</a>.</p>
+<?php endif ?>
+
+
 
 <div class="oc-blog-post oc-clearAfter" id="post-<?php the_ID(); ?>">
 
