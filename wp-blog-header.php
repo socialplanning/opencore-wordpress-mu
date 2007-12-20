0c30256f9b3bfed22227a0539feed51b31104f30
Joshua Bronson <jabronson@gmail.com>
2007-12-20 17:09:28 +0000

    minor clean up
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/trunk@12083 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,13 +1,19 @@
 <?php
 
 if (! isset($wp_did_header)):
+
 /* TOPP change -- force setup when a file exists, since we write our
    own wp-config.php */
-if ( !file_exists( dirname(__FILE__) . '/wp-config.php') 
-     || file_exists(dirname(__FILE__) . '/../wp-setup-database.txt')) {
-	if (strpos($_SERVER['PHP_SELF'], 'wp-admin') !== false) $path = '';
-	else $path = 'wp-admin/';
-	$db_filename = dirname(__FILE__) . '/../wp-setup-database.txt';
+
+$config_filename = dirname(__FILE__) . '/wp-config.php';
+$db_filename = dirname(__FILE__) . '/../wp-setup-database.txt';
+
+if (!file_exists($config_filename) || file_exists($db_filename) {
+	if (strpos($_SERVER['PHP_SELF'], 'wp-admin') !== false) {
+        $path = '';
+    } else {
+        $path = 'wp-admin/';
+    }
 	if (file_exists($db_filename)) {
 		$_POST['action'] = 'step3';
 	}
@@ -21,7 +27,7 @@ if ( !file_exists( dirname(__FILE__) . '/wp-config.php')
 
 $wp_did_header = true;
 
-require_once( dirname(__FILE__) . '/wp-config.php');
+require_once($config_filename);
 
 wp();
 gzip_compression();
