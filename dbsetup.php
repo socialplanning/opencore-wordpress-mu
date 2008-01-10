f82474c8e865ccecf8b01011f8cc6bfb93b858b1
Ian Bicking <ianb@colorstudy.com>
2008-01-10 23:22:16 +0000

    Check for the secret on argv in addition to POST arguments
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/trunk@12548 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- dbsetup.php
+++ dbsetup.php
@@ -3,7 +3,13 @@
 define('WP_INSTALLING', true);
 require_once(dirname(__FILE__) . '/wp-config.php');
 require_once(dirname(__FILE__) . '/openplans-auth.php');
-if ($_POST['secret'] != get_openplans_secret()) {
+if ($_SERVER['argv']) {
+    if ($_SERVER['argv'][1] != get_openplans_secret()) {
+        header('Status: 400 Bad Request');
+        die("Invalid secret: '".$_SERVER['argv'][1]."'\n");
+        return 255;
+    }
+} else if ($_POST['secret'] != get_openplans_secret()) {
     header('Status: 400 Bad Request');
     die("Invalid secret: '".$_POST['secret']."'\n");
 }
