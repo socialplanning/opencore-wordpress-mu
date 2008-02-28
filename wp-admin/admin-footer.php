3ba7f3a702a89f493f6d2bb04ba4a26d7c8a3e07
Chris Patterson <cpatterson@openplans.org>
2008-02-28 15:18:55 +0000

    * WP admin footer and adminmenu changes - [re #2194]
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/trunk@13814 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -1,5 +1,5 @@
 
-<div id="footer">
+<div id="wp-footer">
 <p class="logo"><a href="http://wordpress.org/" id="wordpress-logo"><img src="images/wordpress-logo.png" alt="WordPress" /></a></p>
 <p class="docs"><?php _e('<a href="http://codex.wordpress.org/">Documentation</a>'); ?> &#8212; <?php _e('<a href="http://wordpress.org/support/">Support Forums</a>'); ?><br />
 <?php bloginfo('version'); ?> &#8212; <?php printf(__('%s seconds'), timer_stop(0, 2)); ?></p>
