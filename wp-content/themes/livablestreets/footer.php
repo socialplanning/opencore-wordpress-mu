05912cc346cc569f39286589eaa9a7dc23959dab
Paul Winkler <slinkp@gmail.com>
2008-06-09 16:17:23 +0000

    resolve footer.php conflict that seems to have been lingering
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/trunk@18000 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-content/themes/livablestreets/footer.php
+++ wp-content/themes/livablestreets/footer.php
@@ -1,26 +1,14 @@
-    </div><!-- /#content -->
-<?php get_sidebar(); ?>
-<<<<<<< .mine
-  </div>
+   </div><!-- /#content -->
+  <?php get_sidebar(); ?>
+ </div>
 </div><!-- /#main -->
-  <div id="footer">
-    <div class="wrap selfclear">
-=======
+<div id="footer">
+ <div class="wrap selfclear">
+  <?php wp_footer(); ?>
+   <!-- <?php echo $wpdb->num_queries; ?> queries occured in <?php timer_stop(1); ?> seconds. -->
+   <!--<?php do_action('wp_footer'); ?>-->
+ </div>
+</div><!-- /#footer -->
 
-<div id="oc-blog-footer">
-
-<p class="oc-blog-credit oc-discreetText"><!--<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --> </p>
-
-</div><!-- end #oc-blog-footer -->
-
-</div><!-- end #oc-blog-wrapper -->
-
->>>>>>> .r17205
-<?php wp_footer(); ?>
-  <!-- <?php echo $wpdb->num_queries; ?> queries occured in <?php timer_stop(1); ?> seconds. -->
-  <!--<?php do_action('wp_footer'); ?>-->
-</body>
-    </div>
-  </div><!-- /#footer -->
 </body>
 </html>
