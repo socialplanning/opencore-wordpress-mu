65e37b0681a1c3ff92dee3c922a30d0760ba4380
Rob Marianski <rob@marianski.com>
2009-06-10 20:32:14 +0000

    modify before/after widget/title (prevents moving sidebar on no title)
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/branches/feedbacker@24483 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-content/themes/livablestreets/functions.php
+++ wp-content/themes/livablestreets/functions.php
@@ -1,10 +1,10 @@
 <?php
 if ( function_exists('register_sidebar') )
 	register_sidebar(array(
-        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'before_widget' => '<li id="%1$s" class="widget %2$s"><div class="widget-content">',
         'after_widget' => '</div></li>',
         'before_title' => '<div class="widget-header"><h2>',
-        'after_title' => '</h2></div><div class="widget-content">',
+        'after_title' => '</h2></div>',
     ));
 
 ?>
