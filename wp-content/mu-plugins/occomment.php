9cbb7c36aa119f34c4aff5f30eec8894cca6c2b8
Rob Marianski <rob@marianski.com>
2009-07-28 19:41:23 +0000

    no need for occomments to ping opencore specifically because feedbacker handles that
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/branches/feedbacker@25139 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- wp-content/mu-plugins/occomment.php
+++ wp-content/mu-plugins/occomment.php
@@ -43,89 +43,15 @@ var OpenCore = {
             // The user is not a logged in user, so there is no need to continue.
             return;
         }
-        var commentbox = new Ext.Template('<p class="oc-login">',
-'  <input id="oc-log-comment" type="checkbox" checked="checked" />',
-'  Log to my profile',
-'</p>');
+        var commentbox = new Ext.Template('');
         commentbox.append('openplans_info', {id: OpenCore.memberInfo.id,
                          profileurl: OpenCore.memberInfo.profileurl,
                          logouturl:OpenCore.oc_url + 'logout'});
-        Ext.get("commentform").addListener("submit", OpenCore.relaysubmit);
     },
     wp_url: '<?php echo get_option('siteurl'); ?>',
     oc_url: '<?php echo "$Opencore_remote_url"; ?>',
     wp_blogname: '<?php bloginfo('name'); ?>',
     memberInfo: {loggedin: false},
-    /* Simple shortcut for adding a script tag to the page. */
-    addscript: function(scriptSource) {
-        if( document.createElement && document.childNodes ) {
-            var scriptElem = document.createElement('script');
-            scriptElem.setAttribute('type','text/javascript');
-            scriptElem.setAttribute('src',scriptSource);
-            document.getElementsByTagName('head')[0].appendChild(scriptElem);
-        }
-    },
-    /* Handler for comment form submission. */
-    relaysubmit: function(e) {
-        var logCheck = Ext.get('oc-log-comment');
-        if ((logCheck == undefined) || (!logCheck.dom.checked)) {
-            return true;
-        }
-        OpenCore.unhide('oc-progress-spinner');
-        e.preventDefault();
-        // Post comment to WP first, because we care about the comment ID.
-        Ext.Ajax.request({
-            form: Ext.get('commentform').dom,
-            success: OpenCore.wpcomment_onsuccess,
-            failure: OpenCore.wpcomment_onfailure
-            });
-        return true;
-    },
-    wpcomment_onsuccess: function(transport, options) {
-        // Get the URL to the new comment and include that in our form.
-        var commentids = transport.responseText.match(/<li[^>]*id="comment-(\d+)"/g);
-        if (! commentids) {
-            OpenCore.wpcomment_onfailure(transport);
-            return;
-        }
-        var commentid = commentids.slice(-1)[0].match(/<li[^>]*id="comment-(\d+)"/)[1];
-        OpenCore.commenturl = location.href.split('#')[0] + '#comment-' + commentid;
-        var commentform = Ext.get('commentform');
-        var commentElem = document.createElement('input');
-        commentElem.setAttribute('type', 'hidden');
-        commentElem.setAttribute('name', 'commenturl');
-        commentElem.setAttribute('value', OpenCore.commenturl);
-        commentform.appendChild(commentElem);
-        var blognameElem = document.createElement('input');
-        blognameElem.setAttribute('type', 'hidden');
-        blognameElem.setAttribute('name', 'blog_name');
-        blognameElem.setAttribute('value', OpenCore.wp_blogname);
-        commentform.appendChild(blognameElem);
-        // Add a script tag that posts the trackback to openplans,
-        // but persevere eventually if the script source fails to load.
-        OpenCore._submittimer = setTimeout(function() {
-            OpenCore.submitstatus(false,  "Timed out while sending comment to openplans.");}, 10000);
-        OpenCore.addscript('/people/' + Ext.get('user_id').dom.value + '/trackback?' + Ext.Ajax.serializeForm(commentform.dom));
-        return true;
-    },
-    wpcomment_onfailure: function(transport, options) {
-        alert('Posting comment to wordpress failed. Try again later.');
-    },
-    /* Callback that runs after the trackback is posted to openplans. */
-    submitstatus: function(status, msg) {
-        clearTimeout(OpenCore._submittimer);
-        if (! status) {
-            if (msg == undefined) {
-                msg = "unspecified error";
-            }
-            alert(msg);
-        };
-        OpenCore.hide('oc-progress-spinner');
-        location.href = OpenCore.commenturl;
-        // Since that's the same url with a different anchor,
-        // we have to explicitly tell the browser to reload.
-        location.reload();
-    },
     hide: function(name) {
         var hide = Ext.get(name);
         if (hide) hide.addClass('oc-hidden');
