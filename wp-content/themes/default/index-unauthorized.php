104ed7f640967c3facece53abc555d40612a05b5
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-01-24 04:52:42 +0000

    add missing index-unauthorized in default theme
    git-svn-id: https://svn.socialplanning.org/svn/vendor/wordpress-mu/openplans/trunk@26238 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ wp-content/themes/default/index-unauthorized.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+
+<div id="oc-content-main">
+
+
+<p class="oc-message">You are not authorized to see this page.</p>
+
+
+<div class="oc-blog-post oc-clearAfter" id="post-<?php the_ID(); ?>">
+
+</div>
+
+</div><!-- end #oc-blog-main -->
